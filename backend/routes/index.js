5e15ac4afc567536d97a9731d368b24238e268e2
binhcuong <binhcuongpy2014@gmail.com>
2025-10-29 20:14:12 +0700

    Thêm quyền admin ở productRoutes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// router.use('/auth', require('./authRoutes'));       
-// router.use('/users', require('./userRoutes'));      
+router.use('/auth', require('./authRoutes'));       
+router.use('/users', require('./userRoutes'));      
 router.use('/products', require('./productRoutes'));
 router.use('/orders', require('./orderRoutes'));    
 router.use('/order-details', require('./orderDetailRoutes'));
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
 
 router.get('/', productController.getAll);
 router.get('/:id', productController.getById);
-router.post('/', productController.create);
-router.put('/:id', productController.update);
-router.delete('/:id', productController.softDelete);
+
+// Admin only:
+router.post('/', verifyToken, isAdmin, productController.create);
+router.put('/:id', verifyToken, isAdmin, productController.update);
+router.delete('/:id', verifyToken, isAdmin, productController.softDelete);
 
 module.exports = router;
